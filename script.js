4ad15103d2a454fd89da61cc5f7094df7b2cfacf
Lukew101 <109954692+Lukew101@users.noreply.github.com>
2023-01-20 16:29:58 +0100

    function calling made page based

--- script.js
+++ script.js
@@ -6,117 +6,128 @@ const computer = {
     issues: false,
     age: 1.5,
     warrantyChecker: function(age){
-        if(age < 2){
-            return "This computer is still under warranty";
+        if(age > 2){
+            return "No longer under warranty";
         } else{
-            return "This computer is no longer under warranty";
+            return "Still under warranty";
         };
     },
     applications: ["Discord", "Microsoft edge", "Spotify", "Teams", "Visual Studio Code", "Steam", "Google chrome", "Firefox", "Battlenet", "Github Desktop"],
-    applicationChecker: function(application){
-        let appFound = false;
-        for(let i = 0;i<this.applications.length;i++){
-            if(this.applications[i].toLowerCase() === application.toLowerCase()){
-                appFound= true;
-                break;
-            } 
-        }
-        if(appFound){
-            return "This app is on the computer"
-        } else{
-            return "This app is not installed on the computer";
-        };
-    },
-    features: [{
+    features: {
         RAM: 8,
         HDMI: true,
         USBC: true,
         USB: true,
-    }],
-    equipment: [{
+    },
+    equipment: {
         mouse: "Logitech G502 Hero",
         headset: "Logitech Pro Wireless",
         keyboard: "Logitech K120",
         monitor: "ASUS VG255H"
-    }],
+    },
 };
-/*
-console.log(computer.warrantyChecker(computer.age));
-console.log(computer.applicationChecker("discord"));
-*/
 
-if(window.location.pathname === "index.html"){
-    // Log in page
-let hintArrow = document.querySelector(".hint-arrow");
-let hintText = document.querySelector(".hint-text");
+// Log in page
+document.addEventListener("DOMContentLoaded", function(){
+    if(document.querySelector("#index-page")){
+        let hintArrow = document.querySelector(".hint-arrow");
+        let hintText = document.querySelector(".hint-text");
 
-let hintArrowClicked = hintArrow.addEventListener("click", function(){
-    hintText.classList.toggle("show-text");
+        hintArrow.addEventListener("click", function(){
+            hintText.classList.toggle("show-text");
+        });
+
+
+        function checkPassword(){
+            let passwordResult = document.querySelector(".password-result");
+            let passwordInput = document.getElementById("password-textbox");
+            let submitButton = document.querySelector(".enter");
+            let password = "qwerty";
+
+            submitButton.addEventListener("click", function(event){
+                event.preventDefault();
+                if(passwordInput.value == password){
+                    window.location.href = "computer-info.html";
+                } else{
+                    passwordResult.innerHTML = "Incorrect! Please try again.";
+                    setTimeout(function(){
+                        passwordResult.innerHTML = "";
+                    }, 2000);
+                }
+            });
+        };
+        checkPassword();
+    }
 });
 
 
-function checkPassword(){
-    let passwordResult = document.querySelector(".password-result");
-    let passwordInput = document.getElementById("password-textbox");
-    let submitButton = document.querySelector(".enter");
-    let password = "qwerty";
-
-    submitButton.addEventListener("click", function(event){
-        event.preventDefault();
-        if(passwordInput.value == password){
-            window.location.href = "computer-info.html";
-        } else{
-            passwordResult.innerHTML = "Incorrect! Please try again.";
-            setTimeout(function(){
-                passwordResult.innerHTML = "";
-            }, 2000);
-        }
-    });
-};
-checkPassword();
-};
-
-
 // Computer info page
+document.addEventListener("DOMContentLoaded", function(){
+    if(document.querySelector("#computer-info-page")){
+        // Info from object
+        let computerBrand = document.querySelector(".brand");
+        let computerModel = document.querySelector(".model");
+        let computerVersion = document.querySelector(".version");
+        let computerAge = document.querySelector(".age");
+        let computerIssues = document.querySelector(".issues");
+        let computerWarranty = document.querySelector(".warranty");
 
-const applicationCheckerGame = () => {
-    let checkButton = document.querySelector(".check-button");
-    let score = 0;
-    let usedNames = [];
+        computerBrand.innerHTML = computer.brand;
+        computerModel.innerHTML = computer.model;
+        computerVersion.innerHTML = computer.version;
+        computerAge.innerHTML = `${computer.age} years`;
+        if(computer.issues == false){
+            computerIssues.innerHTML = "None";
+        } else {
+            computer.issues = "Yes. These include:";
+        };
+        computerWarranty.innerHTML = computer.warrantyChecker();
 
-    checkButton.addEventListener("click", function(event){
-        event.preventDefault();
 
-        let userWord = document.getElementById("game-textbox").value;
-        let myApps = computer.applications;
-        let gameResult = document.querySelector(".game-result");
-        let appFound = false;
+// App checker game
+        const applicationCheckerGame = () => {
+            let checkButton = document.querySelector(".check-button");
+            let score = 0;
+            let usedNames = [];
 
-        for(let i = 0;i<myApps.length;i++){
-            if(myApps[i].toLowerCase() === userWord.toLowerCase()){
-                appFound= true;
-                break;
-            } 
-        }
-        
-        if(appFound){
-            if(usedNames.includes(userWord.toLowerCase())){
-                gameResult.innerHTML = "Sneaky! You have already used this app. Try again."
-            } else{
-                score++;
-                usedNames.push(userWord.toLowerCase());
-                gameResult.innerHTML = `We have ${score} the same`;
-                if(score >= 2){
-                    gameResult.innerHTML = `We now have ${score} the same. Are you copying me?`;
-                } else if (score >= 3){
-                    gameResult.innerHTML = `We now have ${score} the same. Are you me?`;
-                } else if(score >= 4){
-                    gameResult.innerHTML = `We now have ${score} the same. Now this is weird.`;
+            checkButton.addEventListener("click", function(event){
+                event.preventDefault();
+
+                let userWord = document.getElementById("game-textbox").value;
+                let myApps = computer.applications;
+                let gameResult = document.querySelector(".game-result");
+                let appFound = false;
+
+                for(let i = 0;i<myApps.length;i++){
+                    if(myApps[i].toLowerCase() === userWord.toLowerCase()){
+                        appFound= true;
+                        break;
+                    } 
                 }
-            }
-        } else{
-            gameResult.innerHTML = `Incorrect. We still have ${score} the same`;
-        }
-    });
-};
-applicationCheckerGame();
+        
+                if(appFound){
+                    if(usedNames.includes(userWord.toLowerCase())){
+                        gameResult.innerHTML = "Sneaky! You have already used this app. Try again."
+                    } else{
+                        score++;
+                        usedNames.push(userWord.toLowerCase());
+                        gameResult.innerHTML = `We have ${score} app the same.`;
+                        if(score == 2){
+                            gameResult.innerHTML = `We now have ${score} apps the same. Are you copying me?`;
+                        } else if (score == 3){
+                            gameResult.innerHTML = `We now have ${score} apps the same. Are you me?`;
+                        } else if(score == 4){
+                            gameResult.innerHTML = `We now have ${score} apps the same. This is weird.`;
+                        } else if(score > 4){
+                            gameResult.innerHTML = `We now have ${score} apps the same. You know too much.`;
+                        }
+                    }
+                } else{
+                    gameResult.innerHTML = `Incorrect. We still have ${score} the same.`;
+                }
+                document.getElementById("game-textbox").value = "";
+            });
+        };
+        applicationCheckerGame();
+    }
+});
